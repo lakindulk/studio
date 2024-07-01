15aeb9eff914276a616dde94a2ba501a74ae0a7c
lakindulk <lakindulk9@gmail.com>
2024-07-01 21:41:49 +0530

    update

--- studio/src/Components/ContactDes/ContactDes.jsx
+++ studio/src/Components/ContactDes/ContactDes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Grid, Box } from "@mui/material";
 import CustomTypography from "../Theme/CustomTypography";
-import PhoneInTalkIcon from '@mui/icons-material/PhoneInTalk';
-import EmailIcon from '@mui/icons-material/Email';
+import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+import EmailIcon from "@mui/icons-material/Email";
 
 import "./Contactdes.css";
 
 export default function ContactDes() {
+  const googleMapsUrl = "https://maps.app.goo.gl/S4JhBPTnwSSMabwB9";
+
+  const handleClick = () => {
+    window.open(googleMapsUrl, "_blank", "noopener,noreferrer");
+  };
   return (
     <Grid
       container
@@ -29,7 +34,9 @@ export default function ContactDes() {
             ></Box>
           </Grid>
           <Grid item xs={5} marginLeft="8px">
-            <CustomTypography variant="headerblack">Contact US</CustomTypography>
+            <CustomTypography variant="headerblack">
+              Contact US
+            </CustomTypography>
           </Grid>
         </Grid>
         <Grid marginTop={{ lg: "8px" }} padding={{ lg: "16px" }} zIndex={999}>
@@ -41,58 +48,75 @@ export default function ContactDes() {
             ultrices. Donec nec lacinia leo. Nullam porttitor augue sit amet
             nisi ullamcorper congue. Nam tristique erat id nulla lobortis, ac
             aliquet lorem volutpat. Phasellus nec lacus nec risus tincidunt
-            bibendum. 
+            bibendum.
           </CustomTypography>
-          <CustomTypography pt={2} variant="data2">Our Address </CustomTypography>
-         < CustomTypography variant="navtext2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          <CustomTypography pt={2} variant="data2">
+            Our Address{" "}
           </CustomTypography>
-
-         
+          <CustomTypography variant="navtext2">
+            No 305,Main Street,Kegalle
+          </CustomTypography>
+          <CustomTypography variant="navtext2" marginTop="24px">
+            Find us on google map :{" "}
+          </CustomTypography>
+          <Box  onClick={handleClick} sx={{width:{lg:'500px'},cursor:'pointer',height:{lg:'300px'},marginTop:'16px',
+       '&:hover': {
+          transition: 'transform 0.5s ease, box-shadow 0.6s ease', 
+          boxShadow: `
+            15px 15px 30px #e0e0e0, 
+            -15px -15px 30px #ffffff
+          `, 
+          borderRadius:'20px'
+        },
+        }}>
+            <img onClick={handleClick}  src="https://res.cloudinary.com/iplus/image/upload/v1719848648/Screenshot_2024-07-01_211341_byd97h.png" alt="location" style={{width:'100%',height:'100%',borderRadius:'20px'}}/>
+          </Box>
         </Grid>
       </Grid>
       <Grid item xs={6} pt={8}>
-      <div className="login-box">
-      <form>
-        <div className="user-box">
-          <input type="text" name="username" required />
-          <label>Username</label>
+        <div className="login-box">
+          <form>
+            <div className="user-box">
+              <input type="text" name="username" required />
+              <label>Name</label>
+            </div>
+            <div className="user-box">
+              <input type="text" name="subject" required />
+              <label>Subject</label>
+            </div>
+            <div className="user-box">
+              <input type="text" name="description" required />
+              <label>Description</label>
+            </div>
+            <center>
+              <a href="/#">
+                SEND
+                <span></span>
+              </a>
+            </center>
+          </form>
         </div>
-        <div className="user-box">
-          <input type="password" name="password" required />
-          <label>Password</label>
-        </div>
-        <div className="user-box">
-          <input type="text" name="Inquiery" required />
-          <label>Inquiery</label>
-        </div>
-        <center>
-          <a href="/#">
-            SEND
-            <span></span>
-          </a>
-        </center>
-      </form>
-    </div>     
-      </Grid> 
-      <Grid pt={2} pl={2} container columns={{ xs: 4, sm: 8, md: 12 }} width="800px">
-       
-          <Grid item xs={2} sm={4} md={4} >
-          <PhoneInTalkIcon/>
-          < CustomTypography variant="navtext2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      </Grid>
+      <Grid
+        pt={2}
+        pl={2}
+        container
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        width="800px"
+      >
+        <Grid item xs={2} sm={4} md={4}>
+          <PhoneInTalkIcon />
+          <CustomTypography variant="navtext2">
+           035 2223799
           </CustomTypography>
-         
-          </Grid>
-          <Grid item xs={2} sm={4} md={4} >
-            <EmailIcon/>
-            < CustomTypography variant="navtext2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <EmailIcon />
+          <CustomTypography variant="navtext2">
+          malkastudio305@gmail.com
           </CustomTypography>
-           
-          </Grid>
-      </Grid>  
+        </Grid>
+      </Grid>
     </Grid>
-  
   );
 }
